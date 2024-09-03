814cc9abd57aea36327110d74aad336ea2b84f3f
Aniket Jas <aniketd232@gmail.com>
2024-09-03 13:42:24 +0530

    png-

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,17 +3,17 @@ export default function Social() {
     <div className="flex h-8 w-64">
       <div className="w-1/4">
         <a href="https://github.com/AniketJas" target="_blank">
-          <img src="sociallogo/github.png" alt="" className="h-12" />
+          <img src="sociallogo/github.webp" alt="" className="h-12" />
         </a>
       </div>
       <div className="w-1/4">
         <a href="https://www.linkedin.com/in/aniket-jas/" target="_blank">
-          <img src="sociallogo/linkedin.png" alt="" className="h-12" />
+          <img src="sociallogo/linkedin.webp" alt="" className="h-12" />
         </a>
       </div>
       <div className="w-1/4">
         <a href="https://x.com/aniket_jas_" target="_blank" className="w-1/4">
-          <img src="sociallogo/twitter.png" alt="" className="h-12" />
+          <img src="sociallogo/twitter.webp" alt="" className="h-12" />
         </a>
       </div>
       <div className="w-1/4">
@@ -22,7 +22,7 @@ export default function Social() {
           target="_blank"
           className="w-1/4"
         >
-          <img src="sociallogo/leetcode.png" alt="" className="h-12" />
+          <img src="sociallogo/leetcode.webp" alt="" className="h-12" />
         </a>
       </div>
     </div>
